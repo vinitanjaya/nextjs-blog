82aea4cbc370388bed30633abb2606ee7913117b
appfuxion <appfuxion@appfuxions-MacBook-Air.local>
2026-02-06 15:14:00 +0700

    feat: update home page for preview deployment

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
+        <p>Versi Preview dari branch feature/preiview-home</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
